6650682eb94ed5f1a153d1ce1802178ea6fda053
Misael Adame <misael_adame@protonmail.com>
2022-02-21 15:30:43 -0600

    Rutas en Laravel

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,22 @@ Route::get('/', function () {
 Route::get('cursos', function() {
     return "Bienvenidos a la página cursos";
 });
+
+Route::get('cursos/create', function () {
+    return "En esta página podrás crear un curso";
+});
+
+// Route::get('cursos/{curso}', function($curso) {
+//     return "Bienvenido al curso: $curso";
+// });
+
+                         //puede ser opcional la variable categoria?
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if($categoria) {
+        return "Bienvenido al curso $curso, de la categoría $categoria";
+    } else {
+        return "Bienvenido al curso: $curso";
+    }
+});
+
+
