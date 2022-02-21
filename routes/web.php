7b05ee636a98f9e9bdc1c29eaa4bf03df781c7f2
Misael Adame <misael_adame@protonmail.com>
2022-02-21 16:18:53 -0600

    Controladores

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,30 +15,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    // return view('welcome');
-    return "Bievenido a la página principal";
-});
+Route::get('/', HomeController::class);
 
-Route::get('cursos', function() {
-    return "Bienvenidos a la página cursos";
-});
+// Route::get('/', function () {
+//     // return view('welcome');
+//     return "Bievenido a la página principal";
+// });
 
-Route::get('cursos/create', function () {
-    return "En esta página podrás crear un curso";
-});
+
+Route::get('cursos', [CursoController::class, 'index']);
+
+// Route::get('cursos', function() {
+//     return "Bienvenidos a la página cursos";
+// });
+
+Route::get('cursos/create', [CursoController::class, 'create']);
+
+// Route::get('cursos/create', function () {
+//     return "En esta página podrás crear un curso";
+// });
+
+Route::get('cursos/{curso}', [CursoController::class, 'show']);
 
 // Route::get('cursos/{curso}', function($curso) {
 //     return "Bienvenido al curso: $curso";
 // });
 
                          //puede ser opcional la variable categoria?
-Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
-    if($categoria) {
-        return "Bienvenido al curso $curso, de la categoría $categoria";
-    } else {
-        return "Bienvenido al curso: $curso";
-    }
-});
+// Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+//     if($categoria) {
+//         return "Bienvenido al curso $curso, de la categoría $categoria";
+//     } else {
+//         return "Bienvenido al curso: $curso";
+//     }
+// });
 
 
