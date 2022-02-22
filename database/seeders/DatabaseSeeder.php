219baf54d7c36c51caeaf95f0dfc878c0a84408d
Misael Adame <misael_adame@protonmail.com>
2022-02-22 17:48:52 -0600

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,5 +16,31 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // $curso = new Curso();
+
+        // $curso->name = "Laravel";
+        // $curso->descripcion = "El mejor framework de PHP";
+        // $curso->categoria = "Desarrollo Web";
+
+        // $curso->save();
+
+        // $curso2 = new Curso();
+
+        // $curso2->name = "Laravel";
+        // $curso2->descripcion = "El mejor framework de PHP";
+        // $curso2->categoria = "Desarrollo Web";
+
+        // $curso2->save();
+
+        // $curso3 = new Curso();
+
+        // $curso3->name = "Laravel";
+        // $curso3->descripcion = "El mejor framework de PHP";
+        // $curso3->categoria = "Desarrollo Web";
+
+        // $curso3->save();
+
+        $this->call(CursoSeeder::class);
     }
 }
