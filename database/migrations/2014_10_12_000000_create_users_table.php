d5395ec37c7595b5ebbbb7d9bc0cd8ea7927f596
Misael Adame <misael_adame@protonmail.com>
2022-02-22 12:05:23 -0600

    Introduccion a las migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id(); // Integer Unsigned Increment
+            $table->string('name'); //varchar
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();//created_at updated_at
         });
     }
 
