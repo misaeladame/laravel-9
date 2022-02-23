ba312f8ef4bcabc8b18cea312c39f17b3db19a3d
Misael Adame <misael_adame@protonmail.com>
2022-02-23 11:39:40 -0600

    Agregar y actualizar registros desde un formulario

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -11,7 +11,7 @@ class CursoController extends Controller
         // return "Bienvenidos a la página cursos";
 
         //$cursos = Curso::all();
-        $cursos = Curso::paginate();
+        $cursos = Curso::orderBy('id', 'desc')->paginate();
 
         // return $cursos;
         return view('cursos.index', compact('cursos'));
@@ -20,17 +20,42 @@ class CursoController extends Controller
     public function create() {
         // return "En esta página podrás crear un curso";
         return view('cursos.create');
-
     }
 
-    public function show($id) {
+    public function store(Request $request) {
+        $curso = new Curso();
+
+        $curso->name = $request->name;
+        $curso->descripcion = $request->descripcion;
+        $curso->categoria = $request->categoria;
+
+        $curso->save();
+
+                                            // $curso->id
+        return redirect()->route('cursos.show', $curso);
+    }
+
+    public function show(Curso $curso) {
         // return "Bienvenido al curso $curso";
-
-        $curso = Curso::find($id);
-
         // return view('cursos.show', ['curso' => $curso]);
         return view('cursos.show', compact('curso'));
+    }
 
+                        // //$id
+    public function edit(Curso $curso) {
+        // // $curso = Curso::find($id);
+        // // return $curso;
+        // return $curso;
+        return view('cursos.edit', compact('curso'));
+    }
 
+    public function update(Request $request, Curso $curso) {
+        $curso->name = $request->name;
+        $curso->descripcion = $request->descripcion;
+        $curso->categoria = $request->categoria;
+
+        $curso->save();
+
+        return redirect()->route('cursos.show', $curso);
     }
 }
--- resources/views/cursos/create.blade.php
+++ resources/views/cursos/create.blade.php
@@ -4,6 +4,33 @@
 
 @section('content')
     <h1>En esta página podrás crear un curso</h1>
+
+    <form action="{{route('cursos.store')}}" method="POST">
+
+        @csrf
+
+        <label>
+            Nombre:
+            <br>
+            <input type="text" name="name">
+        </label>
+
+        <br>
+        <label>
+            Descripcion:
+            <br>
+            <textarea name="descripcion" rows="5"></textarea>
+        </label>
+
+        <br>
+        <label>
+            Categoria:
+            <br>
+            <input type="text" name="categoria">
+        </label>
+        <br>
+        <button>Enviar Formulario</button>
+    </form>
 @endsection
 
 
